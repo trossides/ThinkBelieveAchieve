13f42e042199e436fba2af57c5e6b7244bedc155
trossides <theorossides@gmail.com>
2017-08-13 16:30:27 +0930

    Add: Affirmation, Motivation and Master Mind pages.

--- ThinkBelieveAchieve/ThinkBelieveAchieve/MainPage.xaml.cs
+++ ThinkBelieveAchieve/ThinkBelieveAchieve/MainPage.xaml.cs
@@ -29,5 +29,23 @@ namespace ThinkBelieveAchieve
             Detail = new NavigationPage(new VisionBoard());
             IsPresented = false;
         }
+
+        private void Affirmation(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Affirmation());
+            IsPresented = false;
+        }
+
+        private void Motivation(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Motivation());
+            IsPresented = false;
+        }
+
+        private void Master_Mind(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new MasterMind());
+            IsPresented = false;
+        }
     }
 }
